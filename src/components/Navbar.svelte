f0837759bd072d2e07176b2ad7a47251107dd946
william <wayleemh@gmail.com>
2023-12-08 12:26:38 -0500

    add components and basic routes

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Router, Link } from 'svelte-routing'
+</script>
+
+<div class="w-full h-16 shadow flex items-center justify-between px-8 sticky">
+  <h1 class="text-xl">Function</h1>
+  <div class="flex flex-row space-x-10">
+    <Router>
+      <Link to="/">Home</Link>
+      <Link to="/catalog">Catalog</Link>
+      <Link to="/support">Support</Link>
+    </Router>
+  </div>
+  <div class="flex flex-row">
+    <nav>
+      <!-- change to link later -->
+      <div>Log in</div>
+    </nav>
+  </div>
+</div>
---
+++ src/components/pages/Support.svelte
@@ -0,0 +1,4 @@
+<div>
+  <h1>Hello from Support</h1>
+  <p>placeholder</p>
+</div>
